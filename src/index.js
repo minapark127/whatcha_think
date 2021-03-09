490071d9755e5c0de71e4344a5637a8655b7b0b0
Mina <mina.park127@gmail.com>
2021-03-09 16:34:16 +1100

    build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import dotenv from "dotenv";
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
@@ -9,7 +11,7 @@ const server = new GraphQLServer({
   resolvers,
 });
 
-const options = { port: 4000 };
+const options = { port: process.env.PORT || 4000 };
 
 server.start(options, () =>
   console.log(`✅ server is running on localhost:${options.port}`)
